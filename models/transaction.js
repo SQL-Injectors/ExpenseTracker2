1a28f857b3832ee9d10e115246b288e36c1f0c96
k23kumar <k23kumar@gmail.com>
2018-09-08 13:57:38 -0700

    details page

--- models/transaction.js
+++ models/transaction.js
@@ -1,11 +1,11 @@
-// module.exports = function(sequelize, DataTypes) {
-//   var Transaction = sequelize.define("Transaction", {
-//     user_ID: DataTypes.INTEGER, autoIncrement: true,
-//     transaction_ID: DataTypes.INTEGER, autoIncrement: true,
-//     date: DataTypes.DATE,
-//     description: DataTypes.STRING,
-//     category: DataTypes.STRING,
-//     amount: DataTypes.DOUBLE
-//   });
-//   return Transaction;
-// };
+module.exports = function(sequelize, DataTypes) {
+  var Transaction = sequelize.define("Transaction", {
+    user_ID: DataTypes.INTEGER,// autoIncrement: true,
+    transaction_ID: DataTypes.INTEGER, autoIncrement: true,
+    date: DataTypes.DATE,
+    description: DataTypes.STRING,
+    category: DataTypes.STRING,
+    amount: DataTypes.DOUBLE
+  });
+  return Transaction;
+};
