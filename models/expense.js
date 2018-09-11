d278e7b5b09887b7f4c4beda3a93d1acfb146815
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-09-10 23:08:27 -0700

    added expense data rendering to expense details page

--- models/expense.js
+++ models/expense.js
@@ -2,14 +2,14 @@ module.exports = function(sequelize, DataTypes) {
   var Expense = sequelize.define("Expense", {
     date: DataTypes.DATE,
     description: DataTypes.STRING,
-    // category: DataTypes.STRING,
-    amount: DataTypes.DOUBLE
+    category: DataTypes.STRING,
+    amount: DataTypes.DECIMAL(8.2)
   });
 
-  Expense.associate = function(models) {
-    Expense.belongsTo(models.User, {});
-    Expense.belongsTo(models.Category, {});
-  }
+  // Expense.associate = function(models) {
+  //   Expense.belongsTo(models.User, {});
+  //   Expense.belongsTo(models.Category, {});
+  // }
   
   return Expense;
 };
